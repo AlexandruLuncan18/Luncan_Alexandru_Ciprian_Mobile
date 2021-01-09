3063c90f9e939732271ce146c5319891dfac5b93
Luncan Alexandru <alex.luncan18@gmail.com>
2021-01-09 17:54:31 +0200

    Add project files.

---
+++ Luncan_Alexandru_Ciprian_Mobile/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Luncan_Alexandru_Ciprian_Mobile.Data;
+using System.IO;
+
+namespace Luncan_Alexandru_Ciprian_Mobile
+{       
+    public partial class App : Application
+    {
+        static AgendaDatabase database;
+        public static AgendaDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new AgendaDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                    LocalApplicationData), "Notes.db3"));
+                }
+                return database;
+            }
+        }
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new AgendaPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
